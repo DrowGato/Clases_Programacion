a94b166af6c2d58d82013e9647047f97473612ac
DrowGato <129217529+DrowGato@users.noreply.github.com>
2023-06-27 23:57:04 -0400

    Add files via upload

---
+++ Guias/Guia Ejercicio Phyton N°1/ejercicio10.py
@@ -0,0 +1,10 @@
+agenda = {
+    'Nombre': "Juan Pérez",
+    'Direccion': "Calle Falsa 123",
+    'Ciudad': "Osorno",
+    'Numero telefonico': "12345678902"
+}
+agenda['Redes Sociales'] = ['juanperezfb', 'juampi_insta', 'jperez_twitter']
+
+print('Perfil de Facebook:', agenda['Redes Sociales'][0])
+print('Agenda:', agenda)
