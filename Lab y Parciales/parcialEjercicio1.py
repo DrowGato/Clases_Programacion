9a3092ffdd46200cea27b28e8c2354bf28a30303
DrowGato <129217529+DrowGato@users.noreply.github.com>
2023-06-28 20:16:51 -0400

    Delete Lab y Parciales directory

--- Lab y Parciales/parcialEjercicio1.py
+++
@@ -1,9 +0,0 @@
-###PaLINDROMO####
-def palindromo(palabra):
-    return palabra == palabra[::-1]
-
-palabra = input("ingrese una palabra: ")
-if palindromo(palabra):
-    print(f"{palabra} si es un palindromo")
-else:
-    print(f"{palabra} no es un palindromo")
